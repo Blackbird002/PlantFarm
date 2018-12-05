c80134d07209299662175c994c96bd2ae4080022
sonofmath <sonofmath3.14@hotmail.com>
2018-12-05 11:38:47 -0700

    Added Update Plant Activity and navigation button

---
+++ app/src/main/java/com/example/riads/plantfarm/UpdatePlantActivity.java
@@ -0,0 +1,13 @@
+package com.example.riads.plantfarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UpdatePlantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_plant);
+    }
+}
