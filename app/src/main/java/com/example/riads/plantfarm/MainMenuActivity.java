bce8e949e9203930b3d46d96762b63e02eddf9ae
Riad Shash <riadshash@yahoo.com>
2018-12-08 23:32:40 -0700

    Added a Pre-Start class that extends android.app.Application to enable persistance with firebase.

--- app/src/main/java/com/example/riads/plantfarm/MainMenuActivity.java
+++ app/src/main/java/com/example/riads/plantfarm/MainMenuActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainMenuActivity extends AppCompatActivity {
 
     @Override
