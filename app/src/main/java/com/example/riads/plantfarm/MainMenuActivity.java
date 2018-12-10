da372037bca5eb98636cd21e27385936454813a3
sonofmath <sonofmath3.14@hotmail.com>
2018-12-10 00:31:30 -0700

    Authentication working. Still needs email verification and User update info

--- app/src/main/java/com/example/riads/plantfarm/MainMenuActivity.java
+++ app/src/main/java/com/example/riads/plantfarm/MainMenuActivity.java
@@ -1,16 +1,45 @@
 package com.example.riads.plantfarm;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainMenuActivity extends AppCompatActivity {
+    private FirebaseAuth.AuthStateListener authListener;
+    private FirebaseAuth auth;
+    private Button signOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        //get firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
+        //get current user
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user == null) {
+                    // user auth state is changed - user is null
+                    // launch login activity
+                    startActivity(new Intent(MainMenuActivity.this, LoginActivity.class));
+                    finish();
+                }
+            }
+        };
+
+        signOut = (Button) findViewById(R.id.btnSignOut);
     }
 
     //Goes to the Create Activity
@@ -33,6 +62,12 @@ public class MainMenuActivity extends AppCompatActivity {
     public void goToPlantLogs(View v){
         Intent intent = new Intent(this, PlantLogsActivity.class);
         startActivity(intent);
+    }
+
+
+    public void signOut(View v) {
+        auth.signOut();
+        startActivity(new Intent(MainMenuActivity.this, LoginActivity.class));
     }
 
 
